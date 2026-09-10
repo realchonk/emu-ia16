fa95f39b2b4b8b5e563879e6dd7f3e62dcc0f597
Benjamin Stürz <benni@stuerz.xyz>
2026-09-10 21:39:11 +0200

    bin/hello: use new libc features

--- bin/hello/hello.c
+++ bin/hello/hello.c
@@ -1,11 +1,45 @@
-#include <stddef.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <err.h>
 
 int
-main ()
+main (argc, argv)
+int	  argc;
+char	**argv;
 {
-	puts ("Hello World");
-	printf ("%s %d\n", "Hello World", 42);
+	char	*string;
+	int	 option, x;
+
+	string = "Hello World";
+	x = 0;
+
+	while ((option = getopt (argc, argv, "e:s:x")) != -1) {
+		switch (option) {
+		case 'e':
+			errno = atoi (optarg);
+			err (1, "-e option specified");
+		case 's':
+			string = optarg;
+			break;
+		case 'x':
+			++x;
+			break;
+		default:
+			return 1;
+		}
+	}
+
+	argv += optind;
+	argc -= optind;
+
+	printf ("%s (%d)\n", string, x);
+
+	if (argc != 0) {
+		for (x = 0; x < argc; ++x)
+			printf ("[%d]: %s\n", x, argv[x]);
+	}
+
 	return 0;
 }
