98f702f96cdfa8637076271c7549516ccfcef2f7
Benjamin Stürz <benni@stuerz.xyz>
2026-09-10 22:51:28 +0000

    lib/c1: rename irfile -> astfile

--- lib/c1/main.c
+++ lib/c1/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <err.h>
 
-FILE *irfile, *symfile;
+FILE *astfile, *symfile;
 
 
 int
@@ -13,21 +13,23 @@ char	**argv;
 	char buf[4];
 
 	if (argc != 3) {
-		fputs ("usage: c1 irfile symfile\n", stderr);
+		fputs ("usage: c1 astfile symfile\n", stderr);
 		return 1;
 	}
 
-	irfile = fopen (argv[1], "r");
-	if (irfile == NULL)
+	astfile = fopen (argv[1], "r");
+	if (astfile == NULL)
 		err (1, "fopen('%s')", argv[1]);
 
 	symfile = fopen (argv[2], "r");
-	if (irfile == NULL)
+	if (astfile == NULL)
 		err (1, "fopen('%s')", argv[2]);
 
-	if (fread (buf, 1, 4, irfile) != 4)
+	if (fread (buf, 1, 4, astfile) != 4)
 		err (1, "fread()");
 
 	if (memcmp (buf, "CIR", 4) != 0)
 		errx (1, "invalid magic");
+
+	return 0;
 }
