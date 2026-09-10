fd940c387b43e35c64bfcf9e4ebe4a0fe4289f6e
Benjamin Stürz <benni@stuerz.xyz>
2026-09-10 22:30:43 +0000

    libc: remove GCC compat

--- lib/libc/include/stdbool.h
+++ lib/libc/include/stdbool.h
@@ -1,11 +1,8 @@
 #ifndef __bool_true_false_are_defined
 
-# if __GNUC__
-#  include_next <stdbool.h>
-# else
 typedef int bool;
 #  define false 0
-#  define true
+#  define true 1
 #  define __bool_true_false_are_defined 1
-# endif
+
 #endif
--- lib/libc/include/stddef.h
+++ lib/libc/include/stddef.h
@@ -1,15 +1,10 @@
 #ifndef FILE_STDDEF_H
 #define FILE_STDDEF_H
 
-# if __GNUC__
-#  include_next <stddef.h>
-# else
-
 typedef unsigned	size_t;
 typedef int		ptrdiff_t;
 
 #define NULL	0
 #define offsetof(t, m)	((size_t) &((t *) 0)->m)
 
-# endif
 #endif /* FILE_STDDEF_H */
